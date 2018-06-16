6a0598d8cd5b5941a7b735d9f6e9f1ac658174d2
cdur00q <36085133+cdur00q@users.noreply.github.com>
2018-06-16 18:17:33 +0000

    Add files via upload
    First version worth backing up.

---
+++ functions.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include "variables.h"
+#include <vector>
+#include <fstream>
+#include <QString>
+
+
+fileReadResult getGameData(QString target, std::ifstream &actorFile, std::vector<actor> &actors);
+
+#endif // FUNCTIONS_H
